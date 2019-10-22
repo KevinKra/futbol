1029d6d4082289d54e7d65a4cbaa5134bfa14869
Kevin Krato <kratok08@gmail.com>
2019-10-22 14:11:04 -0600

    Write initial tests for Team class

---
+++ test/team_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative "../lib/team.rb"
+
+class TeamTest < Minitest::Test
+  def setup
+    mock_team_data = {
+      team_id: 3,
+      franchiseId: 23,
+      teamName: "ATL United",
+      abbreviation: "ATL",
+      Stadium: "Mercedes-Benz Stadium",
+      link: "/api/v1/teams/1"
+    }
+    @team = Team.new(mock_team_data)
+  end
+
+  def test_it_exists
+    assert_instance_of Team, @team
+  end
+end
