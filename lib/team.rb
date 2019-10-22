2f9c8f8f9c3b318ea80757239ee2ae38c62755ab
Kevin Krato <kratok08@gmail.com>
2019-10-22 12:29:22 -0600

    Resolve csv naming issues

--- lib/team.rb
+++ lib/team.rb
@@ -2,8 +2,8 @@ class Team
 
   def initialize(team_data)
     @team_id = team_data[:team_id]
-    @franchise_id = team_data[:franchise_id]
-    @team_name = team_data[:team_name]
+    @franchise_id = team_data[:franchiseid]
+    @team_name = team_data[:teamname]
     @abbreviation = team_data[:abbreviation]
     @stadium = team_data[:stadium]
     @link = team_data[:link]
