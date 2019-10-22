7efe59813b79ae5e8627816cbd3932ecffb80d8e
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-22 11:53:10 -0600

    Add functionality for csv parsing

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,11 +1,17 @@
 require 'csv'
 require_relative './game_collection'
 require_relative './game'
+require_relative './team_collection'
+require_relative './team'
+require_relative './result_collection'
+require_relative './result'
 
 class StatTracker
 
-  def initialize(game_path, team_path, result_path)
-
+  def initialize(game_data, team_data, result_data)
+    # @game_data = game_data
+    # @team_data = team_data
+    # @result_data = result_data
   end
 
   def self.from_csv(location_paths)
@@ -15,6 +21,7 @@ class StatTracker
     game_data = GameCollection.new(parse_csv_data(game_path))
     team_data = TeamCollection.new(parse_csv_data(team_path))
     result_data = ResultCollection.new(parse_csv_data(result_path))
+    require "pry"; binding.pry
     self.new(game_data, team_data, result_data)
   end
 
