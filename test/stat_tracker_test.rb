78c2299fe75ae9905c3239c405a64b20ef389b5a
Darren Campbell <darren2802@gmail.com>
2019-10-23 15:24:30 -0600

    Fixes setup and initialize tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,38 +1,27 @@
-# require_relative './test_helper'
-# require 'csv'
-# require_relative '../lib/stat_tracker.rb'
-# require_relative '../lib/game_collection'
-# require_relative '../lib/game'
-# require_relative '../lib/team_collection'
-# require_relative '../lib/team'
-# require_relative '../lib/result_collection'
-# require_relative '../lib/result'
+require_relative './test_helper'
+require_relative '../lib/stat_tracker.rb'
 
+class StatTrackerTest < Minitest::Test
+  def setup
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    result_path = './data/results.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      result: result_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
 
+  def test_it_exists
+    assert_instance_of StatTracker, @stat_tracker
+  end
 
-# class StatTrackerTest < Minitest::Test
-#   def setup
-#     game_path = "./mock_data/mock_games.csv"
-#     team_path = "./mock_data/mock_teams.csv"
-#     result_path = "./mock_data/mock_results.csv"
-#     locations = {
-#       games: game_path,
-#       teams: team_path,
-#       result: result_path
-#     }
-#     @stat_tracker = StatTracker.from_csv(locations)
-#   end
+  def test_initialized
+    assert_equal 32, @stat_tracker.team_data.length
+    assert_equal 14882, @stat_tracker.result_data.length
+    assert_equal 7441, @stat_tracker.game_data.length
+  end
 
-#   def test_it_exists
-#     assert_equal [], @stat_tracker
-#   end
-
-#   # def test_it_parses_csv_data_from_given_paths
-
-#   # end
-
-#   # def test_it_parses_csv_data_correctly
-#   #   # mock_results = [{}]
-#   #   assert_equal [], @stat_tracker.parse_csv_data()
-#   # end
-# end
+end
