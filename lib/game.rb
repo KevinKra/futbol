cc58a667d6cab639aa2b882a998f19bb251bfc76
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-23 19:24:31 -0600

    Update attr_readers in Game class file

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,16 @@
 class Game
 
-  attr_reader :game_id, :type
+  attr_reader :game_id,
+              :season,
+              :type,
+              :date_time,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals,
+              :venue,
+              :venue_link
+
 
   def initialize(game_data)
     @game_id = game_data[:game_id]
