8b7e4e0eb34cc215a5ca200e0771eec89603f313
Kevin Krato <kratok08@gmail.com>
2019-10-24 13:13:56 -0600

    Update stat_tracker and tests to match project specs

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -25,16 +25,16 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_can_return_a_percentage_of_home_games_won
-    assert_equal 66.67, @stat_tracker.team_games_percentages(6)
+    assert_equal 66.67, @stat_tracker.percentage_home_wins(6)
 
   end 
 
   def test_it_can_return_a_percentage_of_away_games_won
-    assert_equal 100, @stat_tracker.team_games_percentages(6, "away")
+    assert_equal 100, @stat_tracker.percentage_visitor_wins(6)
   end
 
   def test_it_can_return_a_percentage_of_games_tied
-    assert_equal 33.33, @stat_tracker.team_games_percentages(3, "away", "TIE")
+    assert_equal 33.33, @stat_tracker.percentage_ties(3)
   end
 
 end
