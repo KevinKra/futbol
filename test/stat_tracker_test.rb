1b09fe5f8d85df280344a70eef7b8565dac2387b
Kevin Krato <kratok08@gmail.com>
2019-10-24 07:45:11 -0600

    Complete all tests for team percentage method

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -5,7 +5,7 @@ class StatTrackerTest < Minitest::Test
   def setup
     game_path = './data/games.csv'
     team_path = './data/teams.csv'
-    result_path = './data/results.csv'
+    result_path = './data/mock_data/mock_results.csv'
     locations = {
       games: game_path,
       teams: team_path,
@@ -20,8 +20,21 @@ class StatTrackerTest < Minitest::Test
 
   def test_initialized
     assert_equal 32, @stat_tracker.team_data.length
-    assert_equal 14882, @stat_tracker.result_data.length
+    assert_equal 7, @stat_tracker.result_data.length
     assert_equal 7441, @stat_tracker.game_data.length
+  end
+
+  def test_it_can_return_a_percentage_of_home_games_won
+    assert_equal 66.67, @stat_tracker.team_games_percentages(6)
+
+  end 
+
+  def test_it_can_return_a_percentage_of_away_games_won
+    assert_equal 100, @stat_tracker.team_games_percentages(6, "away")
+  end
+
+  def test_it_can_return_a_percentage_of_games_tied
+    assert_equal 33.33, @stat_tracker.team_games_percentages(3, "away", "TIE")
   end
 
 end
