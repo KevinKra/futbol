2ce052f28e7e6e142c1575214b887a2aca4c20ae
Darren Campbell <darren2802@gmail.com>
2019-10-23 21:34:24 -0600

    Adds methods and tests

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -21,7 +21,7 @@ class StatTracker
     game_data = GameCollection.new(parse_csv_data(game_path, "game"))
     team_data = TeamCollection.new(parse_csv_data(team_path, "team"))
     result_data = ResultCollection.new(parse_csv_data(result_path, "result"))
-    self.new(game_data, team_data, result_data)
+    self.new(game_data.game_data, team_data.team_data, result_data.result_data)
   end
 
   def self.parse_csv_data(file_path, format)
@@ -40,6 +40,29 @@ class StatTracker
       end
     end
     output
+  end
+
+  def count_of_games_by_season
+    season_count = Hash.new(0)
+    @game_data.each { |game| season_count[game.season] += 1 }
+    season_count
+  end
+
+  def average_goals_per_game
+    game_number = 0
+    goal_total = 0
+    @game_data.each do |game|
+      game_number += 1
+      goal_total += game.away_goals + game.home_goals
+    end
+    (goal_total / game_number.to_f).round(2)
+  end
+
+  def average_goals_by_season
+    season_average = Hash.new{|h,k| h[k] = []}
+    @game_data.each { |game| season_average[game.season] << (game.away_goals + game.home_goals) }
+    season_average.each { |key, value| season_average[key] = (value.sum.to_f / value.length).round(2) }
+    require "pry"; binding.pry
   end
 
 end
