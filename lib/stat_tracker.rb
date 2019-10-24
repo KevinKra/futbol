450c74e309a2ef66efaef3b9b06d2c43aa856e1b
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-23 19:13:37 -0600

    Add highest_total_score, lowest_total_score, biggest_blowout methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -21,7 +21,7 @@ class StatTracker
     game_data = GameCollection.new(parse_csv_data(game_path, "game"))
     team_data = TeamCollection.new(parse_csv_data(team_path, "team"))
     result_data = ResultCollection.new(parse_csv_data(result_path, "result"))
-    self.new(game_data, team_data, result_data)
+    self.new(game_data.game_data, team_data.team_data, result_data.result_data)
   end
 
   def self.parse_csv_data(file_path, format)
@@ -42,4 +42,36 @@ class StatTracker
     output
   end
 
+  def highest_total_score
+    max_sum = 0
+    @game_data.each do |game|
+      sum = game.away_goals.to_i + game.home_goals.to_i
+      if sum > max_sum
+        max_sum = sum
+      end
+    end
+    max_sum
+  end
+
+  def lowest_total_score
+    min_sum = 0
+    @game_data.each do |game|
+      sum = game.away_goals.to_i + game.home_goals.to_i
+      if sum < min_sum
+        min_sum = sum
+      end
+    end
+    min_sum
+  end
+
+  def biggest_blowout
+    highest_difference = 0
+    @game_data.each do |game|
+      difference = (game.away_goals.to_i - game.home_goals.to_i).abs
+      if difference > highest_difference
+        highest_difference = difference
+      end
+    end
+    highest_difference
+  end
 end
