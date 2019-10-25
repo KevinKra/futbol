11313eaf526f69b8a3e3ac86d32267d00dbd9d62
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-24 19:40:31 -0600

    Add CSV parsing to Game Collection file

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -4,7 +4,8 @@ require './lib/game_collection'
 class GameCollectionTest < Minitest::Test
 
   def setup
-    @game_collection = GameCollection.new([])
+    game_data = './data/mock_data/mock_games.csv'
+    @game_collection = GameCollection.new(game_data)
   end
 
   def test_it_exists
