40f77fa964c8b0ea8f2005ac12aafef9b2f3aaaf
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-24 19:39:31 -0600

    Add CSV parsing to Result Collection file

--- lib/result_collection.rb
+++ lib/result_collection.rb
@@ -3,7 +3,15 @@ class ResultCollection
   attr_reader :result_data
 
   def initialize(result_data)
-    @result_data = result_data
+    @result_data = create_results(result_data)
   end
 
+  def length
+    @result_data.length
+  end
+
+  def create_results(result_data)
+    csv = CSV.foreach(result_data, headers:true, header_converters: :symbol)
+    csv.map { |row| Result.new(row) }
+  end
 end
