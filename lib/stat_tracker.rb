543b4e08b9cb71c290f2e44ca57998897b5ce107
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-24 19:40:03 -0600

    Simplify StatTracker initialize and methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -7,118 +7,81 @@ require_relative './result_collection'
 require_relative './result'
 
 class StatTracker
+
   attr_reader :game_data, :team_data, :result_data
-  def initialize(game_data, team_data, result_data)
-    @game_data = game_data
-    @team_data = team_data
-    @result_data = result_data
-  end
 
   def self.from_csv(location_paths)
     game_path = location_paths[:games]
     team_path = location_paths[:teams]
     result_path = location_paths[:result]
-    game_data = GameCollection.new(parse_csv_data(game_path, "game"))
-    team_data = TeamCollection.new(parse_csv_data(team_path, "team"))
-    result_data = ResultCollection.new(parse_csv_data(result_path, "result"))
-    self.new(game_data.game_data, team_data.team_data, result_data.result_data)
+    StatTracker.new(game_path, team_path, result_path)
   end
 
-  def self.parse_csv_data(file_path, format)
-    output = []
-    if format == "game"
-      CSV.foreach(file_path, headers: :true, header_converters: :symbol) do |csv_row|
-        output << Game.new(csv_row)
-      end
-    elsif format == "team"
-      CSV.foreach(file_path, headers: :true, header_converters: :symbol) do |csv_row|
-        output << Team.new(csv_row)
-      end
-    elsif format == "result"
-      CSV.foreach(file_path, headers: :true, header_converters: :symbol) do |csv_row|
-        output << Result.new(csv_row)
-      end
-    end
-    output
+  def initialize(game_data, team_data, result_data)
+    @game_data = GameCollection.new(game_data)
+    @team_data = TeamCollection.new(team_data)
+    @result_data = ResultCollection.new(result_data)
   end
 
-  def highest_total_score
-    max_sum = 0
-    @game_data.each do |game|
-      sum = game.away_goals.to_i + game.home_goals.to_i
-      if sum > max_sum
-        max_sum = sum
-      end
-    end
-    max_sum
-  end
 
-  def lowest_total_score
-    min_sum = 0
-    @game_data.each do |game|
-      sum = game.away_goals.to_i + game.home_goals.to_i
-      if sum < min_sum
-        min_sum = sum
-      end
-    end
-    min_sum
-  end
+def highest_total_score
+  @game_data.highest_total_score
+end
 
-  def biggest_blowout
-    highest_difference = 0
-    @game_data.each do |game|
-      difference = (game.away_goals.to_i - game.home_goals.to_i).abs
-      if difference > highest_difference
-        highest_difference = difference
-      end
-    end
-    highest_difference
-  end
+def lowest_total_score
+  @game_data.lowest_total_score
+end
 
-  def games_by_team_id(team_id, format, seek_result)
-    # team_id, format ("home", "away"), seek_result ("WIN", "LOSE", "TIE")
-    all_matching_games = @result_data.select do |game| 
-      game.team_id == team_id.to_s && game.hoa == format
-    end
-    matching_results = all_matching_games.select do |game|
-      game.result == seek_result
-    end.length
-    outcome_percentage = (matching_results.to_f / all_matching_games.length) * 100
-    outcome_percentage.round(2)
-  end
+def biggest_blowout
+  @game_data.biggest_blowout
+end
 
-  def percentage_home_wins(team_id)
-    games_by_team_id(team_id, "home", "WIN")
-  end
-
-  def percentage_visitor_wins(team_id)
-    games_by_team_id(team_id, "away", "WIN")
-  end
-
-  def percentage_ties(team_id)
-    games_by_team_id(team_id, "away", "TIE")
-  end
-
-  def count_of_games_by_season
-    season_count = Hash.new(0)
-    @game_data.each { |game| season_count[game.season] += 1 }
-    season_count
-  end
-
-  def average_goals_per_game
-    game_number = 0
-    goal_total = 0
-    @game_data.each do |game|
-      game_number += 1
-      goal_total += game.away_goals + game.home_goals
-    end
-    (goal_total / game_number.to_f).round(2)
-  end
-
-  def average_goals_by_season
-    season_average = Hash.new{|h,k| h[k] = []}
-    @game_data.each { |game| season_average[game.season] << (game.away_goals + game.home_goals) }
-    season_average.each { |key, value| season_average[key] = (value.sum.to_f / value.length).round(2) }
-  end
+  #
+  # def games_by_team_id(team_id, format, seek_result)
+  #   # team_id, format ("home", "away"), seek_result ("WIN", "LOSE", "TIE")
+  #   all_matching_games = @result_data.select do |game|
+  #     game.team_id == team_id.to_s && game.hoa == format
+  #   end
+  #   matching_results = all_matching_games.select do |game|
+  #     game.result == seek_result
+  #   end.length
+  #   outcome_percentage = (matching_results.to_f / all_matching_games.length) * 100
+  #   outcome_percentage.round(2)
+  # end
+  #
+  # def percentage_home_wins(team_id)
+  #   games_by_team_id(team_id, "home", "WIN")
+  # end
+  #
+  # def percentage_visitor_wins(team_id)
+  #   games_by_team_id(team_id, "away", "WIN")
+  # end
+  #
+  # def percentage_ties(team_id)
+  #   games_by_team_id(team_id, "away", "TIE")
+  # end
+  #
+  # def count_of_games_by_season
+  #   season_count = Hash.new(0)
+  #   @game_data.each { |game| season_count[game.season] += 1 }
+  #   season_count
+  # end
+  #
+  # def average_goals_per_game
+  #   game_number = 0
+  #   goal_total = 0
+  #   @game_data.each do |game|
+  #     game_number += 1
+  #     goal_total += game.away_goals + game.home_goals
+  #   end
+  #   (goal_total / game_number.to_f).round(2)
+  # end
+  #
+  # def average_goals_by_season
+  #   season_average = Hash.new{|h,k| h[k] = []}
+  #   require "pry"; binding.pry
+  #   @game_data.each { |game| season_average[game.season] << (game.away_goals + game.home_goals) }
+  #   season_average.each { |key, value| season_average[key] = (value.sum.to_f / value.length).round(2) }
+  # end
 
 end
