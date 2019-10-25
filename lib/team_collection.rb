037eb9aa7460f96f6731664aadc4da5c5d09f973
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-24 19:40:20 -0600

    Add CSV parsing to Team Collection file

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -3,7 +3,15 @@ class TeamCollection
   attr_reader :team_data
 
   def initialize(team_data)
-    @team_data = team_data
+    @team_data = create_teams(team_data)
   end
 
+  def length
+    @team_data.length
+  end
+
+  def create_teams(team_data)
+    csv = CSV.foreach(team_data, headers:true, header_converters: :symbol)
+    csv.map { |row| Team.new(row) }
+  end
 end
