6e15911eaee0e9efe3360a5cd939d5382049485c
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-26 16:05:58 -0600

    Update team class with CSV parsing and attr_readers

--- lib/team.rb
+++ lib/team.rb
@@ -1,4 +1,12 @@
 class Team
+  @@team_data = []
+
+  attr_reader :team_id,
+              :franchiseid,
+              :team_name,
+              :abbreviation,
+              :stadium,
+              :link
 
   def initialize(team_data)
     @team_id = team_data[:team_id]
@@ -9,12 +17,26 @@ class Team
     @link = team_data[:link]
   end
 
+  def self.assign_team_data(data)
+    @@team_data = data
+  end
+
+  def self.team_data
+    @@team_data
+  end
+
   def self.parse_csv_data(file_path)
     output = []
     CSV.foreach(file_path, headers: :true, header_converters: :symbol) do |csv_row|
       output << Team.new(csv_row)
     end
-    output
+    self.assign_team_data(output)
+  end
+
+  def self.lookup_team_name(id)
+    @@team_data.find do |team|
+      team.team_id == id
+    end.team_name
   end
 
 end
