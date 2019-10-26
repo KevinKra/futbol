c518f69beb28cf31065af765a868ed6c34b5238a
Kevin Krato <kratok08@gmail.com>
2019-10-26 08:31:21 -0600

    Setup team class and write initial methods

--- lib/team.rb
+++ lib/team.rb
@@ -1,4 +1,7 @@
+require 'csv'
+
 class Team
+  @@team_data = []
 
   def initialize(team_data)
     @team_id = team_data[:team_id]
@@ -9,12 +12,24 @@ class Team
     @link = team_data[:link]
   end
 
+  def self.assign_team_data(data)
+    @@team_data = data
+  end
+
+  def self.team_data
+    @@team_data
+  end
+  
   def self.parse_csv_data(file_path)
     output = []
     CSV.foreach(file_path, headers: :true, header_converters: :symbol) do |csv_row|
       output << Team.new(csv_row)
     end
-    output
+    self.assign_team_data(output)
+  end
+
+  def self.count_of_teams
+    @@team_data.length
   end
 
 end
