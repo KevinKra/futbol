9c754ef4db836e4d9215bf14ca757ae4c753b02b
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-26 16:05:38 -0600

    Update stat_tracker with highest_scoring_visitor method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -7,6 +7,7 @@ class StatTracker
   def self.from_csv(location_paths)
     result_data = Result.parse_csv_data(location_paths[:results])
     game_data = Game.parse_csv_data(location_paths[:games])
+    team_data = Team.parse_csv_data(location_paths[:teams])
     StatTracker.new
   end
 
@@ -45,4 +46,11 @@ class StatTracker
   def average_goals_by_season
     Game.average_goals_by_season
   end
+
+  def highest_scoring_visitor
+    highest_average = Result.highest_scoring_visitor
+    require "pry"; binding.pry
+    Team.lookup_team_name(highest_average)
+  end
+
 end
