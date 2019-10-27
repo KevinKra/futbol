247755dd1f7a6d06a5e2358da306adca348de794
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-27 12:21:53 -0600

    Add tests for total_scores, highest_total_score, lowest_total_score, score_difference, and biggest_blowout

--- test/game_test.rb
+++ test/game_test.rb
@@ -17,4 +17,26 @@ class GameTest < Minitest::Test
     assert_equal "2012030321", @game.game_id
     assert_equal "Postseason", @game.type
   end
+
+  def test_total_scores
+    assert_equal 3, Game.total_scores[0]
+  end
+
+  def test_highest_total_score
+    assert_equal 7, Game.highest_total_score
+  end
+
+  def test_lowest_total_score
+    assert_equal 2, Game.lowest_total_score
+  end
+
+  def test_score_difference
+    assert_equal 1, Game.score_difference[0]
+  end
+
+  def test_biggest_blowout
+    assert_equal 3, Game.biggest_blowout
+  end
+
+
 end
