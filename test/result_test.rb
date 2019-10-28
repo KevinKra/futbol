83b8ac735eefbb6f1b4a307e496a5781ee7ae716
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-28 09:52:49 -0600

    Adjust tests to align with mock data

--- test/result_test.rb
+++ test/result_test.rb
@@ -15,21 +15,21 @@ class ResultTest < Minitest::Test
 
   def test_it_has_the_correct_data_amount
     total_data = Result.result_data
-    assert_equal 7, total_data.length
+    assert_equal 20, total_data.length
   end
 
   def test_that_games_by_team_id_works_correctly
-    assert_equal 66.67, Result.games_by_team_id(6, "home", "WIN")
+    assert_equal 100.0, Result.games_by_team_id(6, "home", "WIN")
     assert_equal 100, Result.games_by_team_id(6, "away", "WIN")
-    assert_equal 33.33, Result.games_by_team_id(3, "away", "TIE")
+    assert_equal 0.0, Result.games_by_team_id(3, "away", "TIE")
   end
 
   def test_average_scores_by_type
-    assert_equal 2.0, Result.average_scores("away").values[0][:average_goals]
+    assert_equal 1.67, Result.average_scores("away").values[0][:average_goals]
   end
 
   def test_highest_scoring_visitor
-    assert_equal "3", Result.highest_scoring_visitor
+    assert_equal "6", Result.highest_scoring_visitor
   end
 
   def test_highest_scoring_home_team
@@ -37,19 +37,19 @@ class ResultTest < Minitest::Test
   end
 
   def test_lowest_scoring_visitor
-    assert_equal "6", Result.lowest_scoring_visitor
+    assert_equal "5", Result.lowest_scoring_visitor
   end
 
   def test_lowest_scoring_home_team
-    assert_equal "6", Result.lowest_scoring_home_team
+    assert_equal "5", Result.lowest_scoring_home_team
   end
-  
+
   def test_it_can_determine_the_team_with_most_goals_across_all_seasons
-    assert_equal "3", Result.find_best_offense
+    assert_equal "6", Result.find_best_offense
   end
 
   def test_it_can_determine_the_team_with_least_goals_across_all_seasons
-    assert_equal "6", Result.find_best_offense(false)
+    assert_equal "5", Result.find_best_offense(false)
   end
 
 end
