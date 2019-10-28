9a3fa4208dc9050eae136bf46d928acdad8b33d4
Darren Campbell <darren2802@gmail.com>
2019-10-27 22:27:51 -0600

    Add iteration 3 methods winningest_team best_fans worst_fans

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -4,9 +4,11 @@ require_relative './team'
 require_relative './result'
 
 class StatTracker
+
   def self.from_csv(location_paths)
     result_data = Result.parse_csv_data(location_paths[:results])
     game_data = Game.parse_csv_data(location_paths[:games])
+    team_data = Team.parse_csv_data(location_paths[:teams])
     StatTracker.new
   end
 
@@ -34,15 +36,31 @@ class StatTracker
     Result.games_by_team_id(team_id, "away", "TIE")
   end
 
-  def count_of_games_by_season
+  def count_of_games_by_season # iteration-2-darren
     Game.count_of_games_by_season
   end
 
-  def average_goals_per_game
+  def average_goals_per_game # iteration-2-darren
     Game.average_goals_per_game
   end
 
-  def average_goals_by_season
+  def average_goals_by_season # iteration-2-darren
     Game.average_goals_by_season
   end
+
+  def winningest_team # iteration-3-darren
+    id_best_team = Result.winningest_team
+    Team.lookup_team_name(id_best_team)
+  end
+
+  def best_fans # iteration-3-darren
+    best_fan_team_id = Result.best_worst_fans.max_by { |key, value| value[:diff_home_away_win_pct] }[0]
+    team_name = Team.lookup_team_name(best_fan_team_id)
+  end
+
+  def worst_fans # iteration-3-darren
+    team_ids = Result.best_worst_fans.keep_if { |key, value| value[:diff_home_away_win_pct] < 0 }.keys
+    team_names = team_ids.map { |team_id| Team.lookup_team_name(team_id) }
+  end
+
 end
