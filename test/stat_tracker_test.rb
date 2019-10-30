b37ed452c3525df4875873dd3b5165781c8fc702
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-30 15:25:23 -0600

    Add most_tackles and fewest_tackles tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -185,4 +185,12 @@ class StatTrackerTest < Minitest::Test
     assert_equal "New England Revolution", @stat_tracker.least_accurate_team("20122013")
   end
 
+  def test_most_tackles
+    assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20122013")
+  end
+
+  def test_fewest_tackles
+    assert_equal "New England Revolution", @stat_tracker.fewest_tackles("20122013")
+  end
+
 end
